3c55adde455283faab11c1f8734afb944ed390c3
prashanth <prashanthprince929@gmail.com>
2024-03-13 19:07:17 +0530

    .

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 var settings = document.querySelector(".settings");
 var mode = document.querySelector(".nightmode")
 var like = document.querySelectorAll(".activity-img")
-console.log(like)
-console.log(mode)
+
 
 
 
@@ -17,7 +16,7 @@ function nightmode(){
     mode.classList.toggle("night-mode-on")
     document.body.classList.toggle("theme");
     for( let i of like){
-        console.log(i)
+       
         i.classList.toggle("activity-img1")
     }
     
